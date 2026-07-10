85f88e8dc3b77fa6cfa58a31a17b46af3eeb695a
Maggie <magdalena.hernandez@bellevuecollege.edu>
2026-07-10 07:29:41 +0000

    lab activity 1

--- labwork/lab01-input-output/main.py
+++ labwork/lab01-input-output/main.py
@@ -1,7 +1,7 @@
 # DEV 108 Lab Activity 1
 
-# [7/9/2026]
-# [Magdalena Hernandez]
+# []
+# []
 
 
 # Start writing your code below.
@@ -10,4 +10,17 @@ print("Hello world")
 # to see your output in the terminal window.
 # Good luck! You can do this - please don't use any AI tools to do this assignment. 
 # That violates the AI Policy for this class and puts your learning at a disadvantage. 
+firstname = "Magdalena"
+lastname = "Hernandez"
+print ("DEV 108 - Lab Activity 1/n")
+print ("10 + 55 = ", 10+55)
+print ("/nMy name is", firstname, lastname + ".")
+num1 = 26
+num2 = 2026
 
+print()
+print (num1 , "X" , num2 , "-" , num1 - num2)
+# Do an input and output for practice
+# color = input("/nWhat is you favorite color?")
+# print("nMy favorite color is", purple, ".")
+#  
