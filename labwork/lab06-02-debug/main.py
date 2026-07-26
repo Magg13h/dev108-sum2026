7ac6ca0af142d5e928917564c4fc3a3b94323e01
Maggie <magdalena.hernandez@bellevuecollege.edu>
2026-07-26 21:28:25 +0000

    lab 6

--- labwork/lab06-02-debug/main.py
+++ labwork/lab06-02-debug/main.py
@@ -12,11 +12,12 @@ test_score = 0
 
 while True:
     test_score = input("Enter test score (or 'x' to quit): ")
+
     if test_score != "x":
         test_score = int(test_score)
-        counter += 1
     else:
         break
+    
     if test_score >= 0 and test_score <= 100:
         score_total += test_score
         counter += 1
@@ -24,8 +25,11 @@ while True:
         print("Test score must be from 0 through 100. Score discarded. Try again.")   
 
 # calculate average score
-average_score = round(score_total / counter)
-                
+    if counter > 0:
+        average_score = round(score_total / counter)
+    else:
+        average_score = 0
+
 # format and display the result
 print("======================")
 print("Total Score:", score_total,
